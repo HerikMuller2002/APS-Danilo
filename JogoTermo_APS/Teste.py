665771f6c120295e09449d943444dd6963277422
Herik Muller dos Reis <100804626+HerikMuller2002@users.noreply.github.com>
2022-10-31 11:48:42 -0300

    Delete JogoTermo_APS directory

--- JogoTermo_APS/Teste.py
+++
@@ -1,7 +0,0 @@
-chute = 'herik'
-palavra_sorteada = list('raios')
-lista = []
-for i in range(0, 5 + 1):
-    for j in chute:
-        if j in palavra_sorteada and j[i] != palavra_sorteada[i]:
-
--- JogoTermo_APS/classeidioma.py
+++
@@ -1,7 +0,0 @@
-class Idiomas:
-
-    def lingua(idioma):
-        if idioma == '1':
-            return 'Português'
-        else:
-            return "Inglês"
